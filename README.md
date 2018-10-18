515d32455939b90cd8ff011192b298e4782125b5
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-10-18 17:29:49 +0200

    first ver

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # idb2gdb
 Load function names from an IDA Pro database inside GDB
+
+# commands
+
++ `idb_load <IDB file>` Load function names from an IDA Pro database
++ `idb_list` List all function names and addresses associated to the IDB
++ `idb_solve` Solve an IDB name to its address
++ `idb_break` Set a breakpoint from an IDB name
++ `idb_clean` Delete all loaded names
+
+# install
+
+```
+echo 'source /path/to/idb2gdb.py' >> ~/.gdbinit
+```
---
+++ idb2gdb.py
@@ -0,0 +1,114 @@
+__author__ = "Andrea Fioraldi"
+__copyright__ = "Copyright 2018, Andrea Fioraldi"
+__license__ = "BSD 2-Clause"
+__email__ = "andreafioraldi@gmail.com"
+
+import idb
+import gdb
+
+def _image_base():
+    mappings = gdb.execute("info proc mappings", to_string=True)
+    first_num_pos = mappings.find("0x")
+    return int(mappings[first_num_pos: mappings.find(" ", first_num_pos)], 16)
+
+_ida_names = {}
+
+class IdbloadCommand(gdb.Command):
+    '''
+    Load function names from an IDA Pro database
+    '''
+
+    def __init__(self):
+        super(IdbloadCommand, self).__init__("idb_load", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        global _ida_names
+        self.dont_repeat()
+        
+        with idb.from_file(arg) as db:
+            api = idb.IDAPython(db)
+            base = api.idaapi.get_imagebase()
+            for ea in api.idautils.Functions():
+                _ida_names[api.idc.GetFunctionName(ea)] = ea - base
+
+
+class IdblistCommand(gdb.Command):
+    '''
+    List all function names and addresses associated to the IDB
+    '''
+
+    def __init__(self):
+        super(IdblistCommand, self).__init__("idb_list", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        global _ida_names
+        self.dont_repeat()
+        
+        if len(_ida_names) == 0:
+            return
+        
+        base_addr = _image_base()
+        long_size = gdb.lookup_type("long").sizeof * 2
+        
+        max_name_len = max(map(len, list(_ida_names)))
+        for name in sorted(_ida_names, key=lambda n: _ida_names[n]):
+            print("0x" + ("%x" % (base_addr + _ida_names[name])).zfill(long_size) + " (base+0x%x)" % _ida_names[name] + "    " + name.ljust(max_name_len, " ")   )
+
+
+class IdbsolveCommand(gdb.Command):
+    '''
+    Solve an IDB name to its address
+    '''
+
+    def __init__(self):
+        super(IdbsolveCommand, self).__init__("idb_solve", gdb.COMMAND_DATA)
+
+    def invoke(self, arg, from_tty):
+        global _ida_names
+        self.dont_repeat()
+        
+        try:
+            print("0x%x" % (_image_base() + _ida_names[arg]))
+        except KeyError:
+            print("error: name %s not found" % arg)
+
+class IdbbreakCommand(gdb.Command):
+    '''
+    Set a breakpoint from an IDB name
+    '''
+
+    def __init__(self):
+        super(IdbbreakCommand, self).__init__("idb_break", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        global _ida_names
+        self.dont_repeat()
+        
+        try:
+            addr = _image_base() + _ida_names[arg]
+        except KeyError:
+            print("error: name %s not found" % arg)
+            return
+        
+        gdb.execute("break *0x%x" % addr)
+
+class IdbcleanCommand(gdb.Command):
+    '''
+    Delete all loaded names
+    '''
+
+    def __init__(self):
+        super(IdbcleanCommand, self).__init__("idb_clean", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        global _ida_names
+        self.dont_repeat()
+        
+        _ida_names = {}
+
+
+IdbloadCommand()
+IdblistCommand()
+IdbsolveCommand()
+IdbbreakCommand()
+IdbcleanCommand()
